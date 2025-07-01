18cb6b151b3f1de6de22bdc4f0e716509672b705
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-01 17:42:06 +0300

    fix: use lifespan events

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,13 +4,13 @@ from fastapi import FastAPI
 from .dependencies import create_db_and_tables
 from .routers import quizzes
 
-app = FastAPI()
-
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     create_db_and_tables()
     yield
 
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(quizzes.router)
