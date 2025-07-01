aa4ffad71ebc7f15afd295011ca43e6cc19b98a6
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-07-01 23:53:19 +0300

    feat: implement getQuizz api call

--- frontend/src/api/quizzes.ts
+++ frontend/src/api/quizzes.ts
@@ -3,8 +3,11 @@ import { api } from './client';
 
 import type { Quiz, CreateQuiz } from '@api/types';
 
-export const getGroups = () =>
+export const getQuizzes = () =>
     api.get<Quiz[]>('quizzes').then((res) => res.data)
+
+export const getQuiz = (slug: string) =>
+    api.get<Quiz>(`quizzes/${slug}`).then((res) => res.data)
 
 export const createQuiz = (quiz: CreateQuiz) =>
     api.post<Quiz, AxiosResponse<Quiz>, CreateQuiz>('quizzes/', quiz).then((res) => res.data)
